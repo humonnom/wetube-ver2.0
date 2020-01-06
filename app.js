fa53c0f41f3078dd2d2c7931c2b1e5acfac1b7bd
homonnom <josojoso0702@gmail.com>
2020-01-06 20:59:46 +0900

    change location

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`🐬  Listening on: http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
+
+app.get("/", handleHome);
+
+function handleHome(req, res) {
+  res.send("Hi!!!!");
+}
